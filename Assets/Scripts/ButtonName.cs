ec465423fd9990055bb99d7ea1a32b2540d31468
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-02-16 00:16:11 +0100

    Add moving stuff

---
+++ Assets/Scripts/ButtonName.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts
+{
+    internal class ButtonName
+    {
+        internal static string Accelerate = "Accelerate";
+
+        internal static string Brakes = "Brakes";
+    }
+}
---
+++ Assets/Scripts/Car2DController.cs
@@ -0,0 +1,50 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Car2DController : MonoBehaviour
+    {   
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        [UsedImplicitly]
+        private void Start()
+        {
+        
+        }
+
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        [UsedImplicitly]
+        private void Update()
+        {
+            IsAccelerateButtonPressed = Input.GetButton(ButtonName.Accelerate);
+            IsBrakesButtonPressed = Input.GetButton(ButtonName.Brakes);
+        }
+
+        /// <summary>
+        /// Update is called once per physics update
+        /// </summary>
+        [UsedImplicitly]
+        private void FixedUpdate()
+        {
+            if (IsAccelerateButtonPressed)
+            {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * SpeedForce);
+                Debug.Log($"{ButtonName.Accelerate} Button pressed");
+            }
+            if (IsBrakesButtonPressed)
+            {
+                Debug.Log($"{ButtonName.Brakes} Button pressed");
+            }
+        }
+
+        private bool IsAccelerateButtonPressed { get; set; }
+
+        private bool IsBrakesButtonPressed { get; set; }
+
+        private float SpeedForce { get; } = 10;
+    }
+}
