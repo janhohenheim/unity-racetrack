7520f348ff57732a3d907da9f1a5a0bd07896820
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-02-16 00:52:43 +0100

    Set camera to move with car

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform Target;
+
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        [UsedImplicitly]
+        private void Start()
+        {
+
+        }
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        [UsedImplicitly]
+        private void Update()
+        {
+            transform.position = new Vector3(Target.position.x, Target.position.y, -10f);
+        }
+    }
+}
---
+++ Assets/Scripts/InputName.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Scripts
+{
+    internal class InputName
+    {
+        internal static string Accelerate = "Accelerate";
+
+        internal static string Brakes = "Brakes";
+
+        internal static string Horizontal = "Horizontal";
+    }
+}
